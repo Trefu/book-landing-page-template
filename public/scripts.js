d95850b7b4faa418d76523cc4ba77d2e25d6e012
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-06-10 20:52:55 -0300

    test

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,34 @@ document.getElementById("toggleBtn").onclick = function toggleMenu() {
     navToggle.item(i).classList.toggle("hidden");
   }
 };
+
+const AJEDREZ_POEMA = `Apasionante juego tal vez como el desafiante Ajedrez!!\n
+decidida pujante un paso atrás otro para adelante!\n
+Empuñando finas letras, temiendo fueran arrebatar.\n
+Por años cobijando ese tesoro en papel que "lindoslocosrecuerdos" saben entender...\n
+Caminando en aventuras tratando de revelar\n
+aquella nomble escritura que lograste publicar.\n
+Sobre unpqueño libro de cuentos, \n
+Hadas fantasias y más...\n
+sueños majestuosos para contar metióse\n
+la aventura como arañas en telar\n
+al cuento entretejiendo, compartiendo los momentos\n
+desafiando esa mision!!!\n
+Desvelado por las noches\n
+palpitados recuerdos sosteniendo fuerte el timón!\n
+Cómo el arca de Noé, llevando especies de gran valor\n
+metida entre las viñas recibiendo uvas frescas\n
+compartiendo el honor sellando antologías publicadas\n
+con pasión sopla el viento, abren velas...\n
+Zarpa el barco, el viaje empieza\n
+con coraje y solo un lápiez de equipaje\n
+conociendo personajes suben duendes a babor\n
+trazando el desnivel para llegar hasta "Rey"\n
+tu tesoro de papel!!!\n
+`;
+
+window.onload = () => {
+  console.log("pag cargada");
+  var poemaNodo = document.getElementById("test");
+  poemaNodo.textContent = AJEDREZ_POEMA;
+};
