d5eec14a5b04fc27505c7cd474724941264640d5
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-07-03 21:37:39 -0300

    folder poemario,footer

--- public/scripts.js
+++ public/scripts.js
@@ -2,16 +2,7 @@ const toggleBtn = document.getElementById("toggleBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 const VERSE_HEADER = document.getElementById("verse");
 const navToggle = document.getElementsByClassName("toggle");
-/* 
-toggleBtn.onclick = function toggleMenu() {
-  const navToggle = document.getElementsByClassName("toggle");
-  for (let i = 0; i < navToggle.length; i++) {
-    navToggle.item(i).classList.toggle("hidden");
-  }
 
-  mobileMenu.classList.toggle("transform");
-};
- */
 toggleBtn.addEventListener("click", () => {
 
   for (let i = 0; i < navToggle.length; i++) {
@@ -20,17 +11,14 @@ toggleBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("transform");
 });
 
-const btnNavBar = document.getElementsByClassName("btn-navbar");
+const BTNS_NAVBAR = document.getElementsByClassName("btn-navbar");
 
-Array.from(btnNavBar).forEach(function (element) {
-  element.addEventListener("click", () => {
+Array.from(BTNS_NAVBAR).forEach((el) => {
+  el.addEventListener("click", () => {
     toggleBtn.click()
   })
 });
-/* btnNavBar.addEventListener("click", () => {
-  navToggle.click()
-})
- */
+
 
 
 const fadeOutHeader = () => {
@@ -46,37 +34,11 @@ const fadeOutHeader = () => {
 };
 
 const init = () => {
-  setTimeout(() => {
-    document.body.classList.remove("overflow-hidden");
-    const loader = document.getElementById("loading");
-    loader.remove();
+  document.body.classList.remove("overflow-hidden");
+  const loader = document.getElementById("loading");
+  loader.remove();
+  if (VERSE_HEADER) {
     fadeOutHeader();
-  }, 1500);
+  }
 };
-document.o
-nload = init();
-
-/* const AJEDREZ_POEMA = `Apasionante juego tal vez como el desafiante Ajedrez!!\n
-decidida pujante un paso atrás otro para adelante!\n
-Empuñando finas letras, temiendo fueran arrebatar.\n
-Por años cobijando ese tesoro en papel que "lindoslocosrecuerdos" saben entender...\n
-Caminando en aventuras tratando de revelar\n
-aquella nomble escritura que lograste publicar.\n
-Sobre unpqueño libro de cuentos, \n
-Hadas fantasias y más...\n
-sueños majestuosos para contar metióse\n
-la aventura como arañas en telar\n
-al cuento entretejiendo, compartiendo los momentos\n
-desafiando esa mision!!!\n
-Desvelado por las noches\n
-palpitados recuerdos sosteniendo fuerte el timón!\n
-Cómo el arca de Noé, llevando especies de gran valor\n
-metida entre las viñas recibiendo uvas frescas\n
-compartiendo el honor sellando antologías publicadas\n
-con pasión sopla el viento, abren velas...\n
-Zarpa el barco, el viaje empieza\n
-con coraje y solo un lápiez de equipaje\n
-conociendo personajes suben duendes a babor\n
-trazando el desnivel para llegar hasta "Rey"\n
-tu tesoro de papel!!!\n
-`; */
+document.onload = init();
